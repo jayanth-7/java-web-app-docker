8d4c7d131e2c4844c533e72f614dccc2165914a9
jayanth <70301957+jayanth-7@users.noreply.github.com>
2023-03-06 15:38:27 +0530

    updated content

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Hey Jayanth, We are now in Demo server.";
+			return "Hey Jayanth, We are now in Demo server. You've successfully deployed application.";
 		}else{
 			return "Hello " + name;
 		}
